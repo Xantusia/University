ef5b57ef8f9842b56ded9037c726e5789c2e97da
renessansproject <renessansproject@yandex.ru>
2019-10-01 16:24:06 +0300

    5-6 lection

---
+++ ЯП/лекция 5-6 практика/Lection_6-practicum/Lection_6-practicum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection_6_practicum
+{
+    class Person
+    {
+        private string _FName;
+        private string _LName;
+        private DateTime _DateOfBirth;
+        public Person(string FName, string LName, DateTime dateOfBirth)
+        {
+            _FName = FName;
+            _LName = LName;
+            _DateOfBirth = dateOfBirth;
+        }
+        public Person()
+        {
+            _FName = null;
+            _LName = null;
+            _DateOfBirth = new DateTime (0,0,0);
+        }
+        string GetFName() { return _FName; }
+        string GetLName() { return _LName; }
+        DateTime GetDateOfBirth() { return _DateOfBirth; }
+        int GetYearOfBirth() { return _DateOfBirth.Year; }
+        int SetYearOfBirth(int x)
+        { int year = x, month =_DateOfBirth.Month, day = _DateOfBirth.Day;
+          _DateOfBirth = new DateTime(year, month, day);
+          return 0;
+        }
+        public override string ToString()
+        {
+            return _FName + " " + _LName + " was born on " + _DateOfBirth.ToShortDateString();
+        }
+        public string ToShortString()
+        {
+            return _FName + " " + _LName;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps=new Person("Dima","Popov",new DateTime(2000,07,10));
+            Console.WriteLine("Введите количество рядов и столбцов");
+            int nrow = int.Parse(Console.ReadLine());
+            int ncolumn = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ ЯП/лекция 5-6 практика/Lection_6-practicum/Lection_6-practicum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lection_6-practicum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lection_6-practicum")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1f831d3f-55fb-47ea-a4c7-fb41a2f5005e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

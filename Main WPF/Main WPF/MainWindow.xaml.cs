b0c57fd7ec179c67d203443e51f35ccdb485e4e1
renessansproject <renessansproject@yandex.ru>
2020-04-01 18:23:59 +0300

    a

--- Main WPF/Main WPF/MainWindow.xaml.cs
+++ Main WPF/Main WPF/MainWindow.xaml.cs
@@ -12,23 +12,77 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Main_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        Dictionary<string, string> Dict_arg;
         bool debug = true;
         public MainWindow()
         {
+            
             InitializeComponent();
+            Initialize();
+            if(debug)
+            c_Main();
         }
-        public int Start(string argument)
+        public void c_Main()
         {
-
-            return 0;
+            Console.WriteLine(@"Hello, put arguments here, separate by '-'");
+            string args=Console.ReadLine();
+            string [] args_array=args.Split('-');
+            Console.WriteLine('\n');
+            foreach (string arg in args_array)
+            {
+                if (Dict_arg.ContainsKey(arg))
+                    Console.WriteLine(arg + " - " + Dict_arg[arg]);
+                else
+                    Console.WriteLine(arg + " - " + "Don't have this argument");
+            }
+            c_Wait();
+            if (args_array.Contains<string>("SkipLog"))
+                FreeConsole();
+                
         }
-    }
+        public void Initialize()
+        {
+            Dict_arg = new Dictionary<string, string>();
+            Dict_arg.Add("SkipLog", "Closes Console Window");
+        }
+        public void c_Wait()
+        {
+            Console.WriteLine("Press any button...");
+            Console.ReadKey();
+        }
+        public void c_Do(string arg)
+        {
+            switch(arg)
+            {
+                case "debug": { break; }
+                default: { break; }
+            }
+
+        }
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+        IntPtr handle = GetConsoleWindow();
+}
 }
